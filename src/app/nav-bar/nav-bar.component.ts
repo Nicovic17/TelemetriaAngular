e6baebeaf06639ca16e179651266e48fb3c233e3
xerna01 <57638418+xerna01@users.noreply.github.com>
2020-05-31 21:22:57 +0200

    Aggiornata barra di navigazione

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  public inFirstPage;
+  public inSecondPage;
+  public inThirdPage;
+  public inFourthPage;
+  public inFifthPage;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changePage(newpage : number){
+    switch(newpage){
+      case 1:
+        this.inFirstPage="background-color: dimgrey";
+        this.inSecondPage="";
+        this.inThirdPage="";
+        this.inFourthPage="";
+        this.inFifthPage="";
+        break;
+      case 2:
+        this.inFirstPage="";
+        this.inSecondPage="background-color: dimgrey";
+        this.inThirdPage="";
+        this.inFourthPage="";
+        this.inFifthPage="";
+        break;
+      case 3:
+        this.inFirstPage="";
+        this.inSecondPage="";
+        this.inThirdPage="background-color: dimgrey";
+        this.inFourthPage="";
+        this.inFifthPage="";
+        break;
+      case 4:
+        this.inFirstPage="";
+        this.inSecondPage="";
+        this.inThirdPage="";
+        this.inFourthPage="background-color: dimgrey";
+        this.inFifthPage="";
+        break;
+      case 5:
+        this.inFirstPage="";
+        this.inSecondPage="";
+        this.inThirdPage="";
+        this.inFourthPage="";
+        this.inFifthPage="background-color: dimgrey";
+        break;
+    }
   }
 
 }
