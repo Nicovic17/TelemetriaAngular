641c0b5cc891a021c48c6c15f8bab9ade1bbd25c
Nicovic17 <Lcdshop97>
2020-06-01 14:34:18 +0200

    Update Storico

--- src/app/storico/storico.component.ts
+++ src/app/storico/storico.component.ts
@@ -15,9 +15,6 @@ var arrayIdSalvati=[];
 declare var $: any;
 
 
-
-
-
 @Component({
   selector: 'app-storico',
   templateUrl: './storico.component.html',
@@ -40,13 +37,7 @@ export class StoricoComponent implements OnInit {
 
   constructor(public auth: AngularFireAuth, public firebase: AngularFireDatabase) {
 
-
-
     this.checkIfUserIsLogged()
-
-
-
-
   }
 
   ngOnInit(): void {
@@ -73,13 +64,13 @@ export class StoricoComponent implements OnInit {
 
     ref.once("value", snap => {
       snap.forEach(function (child) {
-        //window.alert(child.key+"");
+        
         arrayDate.push(child.key);
       })
 
       document.getElementById("load").innerHTML = "Caricamento completato"
       this.setUpList();
-      //setUpListJS()
+      
     })
 
 
@@ -102,6 +93,142 @@ export class StoricoComponent implements OnInit {
 
 
   public aggiungiData() {
+    var lis = document.getElementById("listaDate").getElementsByTagName("input")
+    var innerText = document.getElementsByName("itemText");
+
+    var i = 0;
+    for (i = 0; i < lis.length; i++) {
+      if (lis[i].checked) {
+        //Aggiungi alle date selezionate
+
+        const index = dateSelezionate.indexOf(innerText[i].innerHTML);
+
+        if (index == -1) //Non ho trovato la data, quindi faccio il push
+        {
+          window.alert("Checked, aggiungo " + innerText[i].innerHTML)
+          dateSelezionate.push(innerText[i].innerHTML);
+          if(dateSelezionate.length==0)
+    {
+      window.alert("Seleziona almeno un istante temporale")
+    }
+    else{
+      window.alert("Vado avanti, carico sensori. Num Date: "+dateSelezionate.length)
+      this.effettuaQueryeMostraSensoriInIstantiDiTempo()
+      document.getElementById("btnJson").style.display="block"
+      document.getElementById("btnAvanti").innerHTML="Aggiorna selezione"
+    }
+        }
+        else{
+          window.alert("Data già presente");
+         
+          
+        }
+      }
+      else {
+        // Se la checkbox è unchecked e la data si trova
+        //nell'array, allora la devo eliminare
+
+
+        const index = dateSelezionate.indexOf(innerText[i].innerHTML);
+        if (index > -1) {//Trovata
+          window.alert("Devo rimuovere" + innerText[i].innerHTML)
+          if (dateSelezionate.length == 1) {//C'è solo una data
+            window.alert("Stai eliminando tutte le selezioni")
+            dateSelezionate = [];
+           
+          }
+          else
+          dateSelezionate.splice(index, 1);
+        }
+
+      }
+    }
+
+    
+    
+
+  }
+
+  effettuaQueryeMostraSensoriInIstantiDiTempo(){
+    //Per ogni data in dateSelezionate recupera gli ID dei sensori
+    //Aggiunge i sensori in arraySensori
+
+    var i=0;
+    document.getElementById("load").innerHTML="Carico sensori..."
+    for(i=0;i<dateSelezionate.length;i++)
+    {
+      var ref = this.firebase.database.ref("Sensori").child(dateSelezionate[i]);
+
+      ref.once("value", snap => {
+        snap.forEach(function (child){
+         window.alert(child.key+"")
+         arraySensori.push(child.key);
+        })
+
+        
+
+        document.getElementById("load").innerHTML="Caricamento sensori completato"
+        
+      })
+
+
+      }
+
+    
+  }
+  
+  numeroSensori: Number;
+  creaJson(){
+
+    window.alert("Sensori caricati: "+arraySensori.length +" arrayDateSelezionate: "+dateSelezionate.length)
+    this.numeroSensori=arraySensori.length/dateSelezionate.length;
+    window.alert("Sensori ogni data: "+ this.numeroSensori)
+
+    window.alert("Dim sens"+ arraySensori.length)
+    var jsonObjectFinale=[];
+    var i=0;
+    for(i=0;i<dateSelezionate.length;i++)
+    {
+      var fine={}
+      fine["tempo"]=dateSelezionate[i];
+      var j=0;
+      for(j=0;j<this.numeroSensori;j++) //Supponendo numero sensori gestiti = numeroSensori
+      {
+        fine["id"+j]=arraySensori[j];
+      }
+
+      jsonObjectFinale.push(fine);
+    }
+
+    var jsonObjectFinaleString=JSON.stringify(jsonObjectFinale);
+    window.alert(jsonObjectFinaleString);
+    var obj=JSON.parse(jsonObjectFinaleString);
+
+    
+    var k=0;
+    $("#listaDate").html("");
+    document.getElementById("btnAvanti").style.display="none"
+    document.getElementById("btnJson").style.display="none"
+    document.getElementById("btnConfermaID").style.display="block"
+    document.getElementById("btnMostraValID").style.display="block"
+    document.getElementById("titleBox").innerHTML="Seleziona ID interessati"
+    for(k=0;k<obj.length;k++)
+    {
+      var fess=0;
+      var ids="";
+      for(fess=0;fess<this.numeroSensori;fess++)
+      {
+        ids+="<input type='checkbox'>ID"+(fess+1)+": <p name='itemText'>"+obj[k]["id"+fess]+"</p>"
+      }
+      //<li><span>Data</span><input type="checkbox"><span>ID</span></li>
+      $("#listaDate").append("<li><span>"+obj[k]["tempo"]+"</span><br>"+ids)
+
+    }
+    
+
+  }
+
+  public aggiungiID() {
     var lis = document.getElementById("listaDate").getElementsByTagName("input")
     var innerText = document.getElementsByName("itemText");
 
@@ -143,76 +270,10 @@ export class StoricoComponent implements OnInit {
       window.alert("Seleziona almeno un istante temporale")
     }
     else{
-      window.alert("Vado avanti, carico sensori"+dateSelezionate.length)
+      window.alert("Vado avanti, carico sensori. Num Date: "+dateSelezionate.length)
       this.effettuaQueryeMostraSensoriInIstantiDiTempo()
       document.getElementById("btnJson").style.display="block"
       document.getElementById("btnAvanti").innerHTML="Aggiorna selezione"
-    }
-    
-
-  }
-
-  effettuaQueryeMostraSensoriInIstantiDiTempo(){
-    //Per ogni data in dateSelezionate recupera gli ID dei sensori
-    //Aggiunge i sensori in arraySensori
-
-    var i=0;
-    document.getElementById("load").innerHTML="Carico sensori..."
-    for(i=0;i<dateSelezionate.length;i++)
-    {
-      var ref = this.firebase.database.ref("Sensori").child(dateSelezionate[i]);
-
-      ref.once("value", snap => {
-        snap.forEach(function (child){
-         // window.alert(child.key+"")
-         arraySensori.push(child.key);
-        })
-
-        document.getElementById("load").innerHTML="Caricamento sensori completato"
-        
-      })
-
-
-      }
-
-    
-  }
-  
-  creaJson(){
-
-    window.alert("Dim sens"+ arraySensori.length)
-    var jsonObjectFinale=[];
-    var i=0;
-    for(i=0;i<dateSelezionate.length;i++)
-    {
-      var fine={}
-      fine["tempo"]=dateSelezionate[i];
-      var j=0;
-      for(j=0;j<2;j++)//Supponendo numero sensori gestiti =2
-      {
-        fine["id"+j]=arraySensori[j];
-      }
-
-      jsonObjectFinale.push(fine);
-    }
-
-    var jsonObjectFinaleString=JSON.stringify(jsonObjectFinale);
-    window.alert(jsonObjectFinaleString);
-    var obj=JSON.parse(jsonObjectFinaleString);
-
-    
-    var k=0;
-    $("#listaDate").html("");
-    document.getElementById("btnAvanti").style.display="none"
-    document.getElementById("btnJson").style.display="none"
-    document.getElementById("btnConfermaID").style.display="block"
-    document.getElementById("btnMostraValID").style.display="block"
-    document.getElementById("titleBox").innerHTML="Seleziona ID interessati"
-    for(k=0;k<obj.length;k++)
-    {
-      //<li><span>Data</span><input type="checkbox"><span>ID</span></li>
-      $("#listaDate").append("<li><span>"+obj[k]["tempo"]+"</span><br><input type='checkbox'>ID1: "+obj[k]["id0"]+"<span></span><input type='checkbox'>ID2: "+obj[k]["id1"]+"<span>")
-
     }
     
 
@@ -230,25 +291,17 @@ export class StoricoComponent implements OnInit {
 
         document.getElementById("user_div").style.display = "none";
         document.getElementById("router").style.display = "block";
-        //document.getElementById("user_div").style.display="block";
-        //document.getElementById("login_div").style.display="none";
-        //document.getElementById("router").style.display="none";
+       
 
       }
       else {
         console.log("Non Loggato");
         document.getElementById("login_div").style.display = "block";
-        //document.getElementById("user_div").style.display = "none";
-        //document.getElementById("login_div").style.display = "block";
-        //document.getElementById("router").style.display="none";
+      
       }
     })
   }
 
-}
-
-function test() {
-  window.alert("TEst")
 }
 
 
