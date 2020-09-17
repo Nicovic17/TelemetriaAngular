e058ab92d13dc011ecfd109cc371074302d3d969
xerna01 <57638418+xerna01@users.noreply.github.com>
2020-09-17 12:49:57 +0200

    Aggiunto modulo per la disattivazione dello scroll della pagina di telemetria

--- src/app/article-index-gallery/article-index-gallery.component.ts
+++ src/app/article-index-gallery/article-index-gallery.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as jQuery from 'jquery';
+
+//Inizio Modulo disattivazione scroll della pagina
+var scrollPosition = [
+  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+];
+var html = jQuery('html');
+html.data('scroll-position', scrollPosition);
+html.data('previous-overflow', html.css('overflow'));
+html.css('overflow', 'hidden');
+window.scrollTo(scrollPosition[0], scrollPosition[1]);
+//Fine modulo
 
 @Component({
   selector: 'app-article-index-gallery',
@@ -10,22 +23,22 @@ import { Component, OnInit } from '@angular/core';
 
 export class ArticleIndexGalleryComponent implements OnInit {
 
-  constructor() { 
+  constructor() {
 
 
   }
 
   ngOnInit(): void {
 
-    
-    
+
+
 
   }
 
-   
 
 
-  
+
+
 
 }
 
