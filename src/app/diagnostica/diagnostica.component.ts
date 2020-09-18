a8912718ee45d296b2b0f9e42ea171bd1a16aa5b
xerna01 <57638418+xerna01@users.noreply.github.com>
2020-09-18 16:38:09 +0200

    Nuove funzionalità Diagnostica... Aggiunti pulsanti per selezionare tutti i messaggi e per deselezionarli.

--- src/app/diagnostica/diagnostica.component.ts
+++ src/app/diagnostica/diagnostica.component.ts
@@ -1,5 +1,5 @@
-import {Component, NgZone, OnInit} from '@angular/core';
-import {MatListModule} from '@angular/material/list';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
+import {MatListModule, MatSelectionList} from '@angular/material/list';
 import {AngularFireAuth} from "@angular/fire/auth";
 import {DiagnosticaService} from "../diagnostica.service";
 
@@ -12,6 +12,8 @@ import {DiagnosticaService} from "../diagnostica.service";
 export class DiagnosticaComponent implements OnInit {
   messagesList: Array<string> = ['root'];
   messageMap = new Map();//Map del tipo <Messaggio>:<Key>
+  @ViewChild('msg', {static: true}) private listObj: MatSelectionList;
+
   constructor(public auth: AngularFireAuth, private _diagService: DiagnosticaService, private ngZone: NgZone) {
     this.checkIfUserIsLogged();
   }
@@ -20,11 +22,17 @@ export class DiagnosticaComponent implements OnInit {
     this.listUpdate();
   }
 
+  selectAll(){
+    this.listObj.selectAll();
+  }
+  selectNone(){
+    this.listObj.deselectAll();
+  }
   listUpdate(){
     this.messagesList.pop();
     this._diagService.getDiagMessages().subscribe(value => {
       this.ngZone.run(() => {
-        this.messagesList.push(value[1]);
+        this.messagesList.unshift(value[1]);
       })
       this.messageMap.set(value[1],value[0]); //L'array conserva l'associazione key-value del db per la cancellazione
       console.log("added "+value);
@@ -34,10 +42,10 @@ export class DiagnosticaComponent implements OnInit {
   deleteMessages(object){
     let index: number;
     for(let i of object){
-      //console.log(i._text.nativeElement.innerText);
-      //console.log(this.messageMap.get(i._text.nativeElement.innerText));
+      console.log(i._text.nativeElement.innerText);
+      console.log(this.messageMap.get(i._text.nativeElement.innerText));
       this._diagService.deleteKey(this.messageMap.get(i._text.nativeElement.innerText));
-      //console.log("Eliminazione: " + this.messageMap.delete(i._text.nativeElement.innerText));
+      console.log("Eliminazione: " + this.messageMap.delete(i._text.nativeElement.innerText));
       index = this.messagesList.indexOf(i._text.nativeElement.innerText);
       this.ngZone.run(() => {
         this.messagesList.splice(index,1);
