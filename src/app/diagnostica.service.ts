b930e0a6887a13d3f238e07ff7a4ceb56265d6c8
xerna01 <57638418+xerna01@users.noreply.github.com>
2020-09-18 13:02:10 +0200

    Completata cancellazione messaggi di diagnostica... A prescindere dalla formattazione che avranno i messaggi, è stata completata l'implementazione di uno o più messaggi di diagnostica alla volta. Ovviamente cancellando un messaggio dalla lista lo si cancella anche dal db

--- src/app/diagnostica.service.ts
+++ src/app/diagnostica.service.ts
@@ -9,13 +9,20 @@ export class DiagnosticaService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  getDiagMessages(): Observable<string>{
-    return new Observable<string>(subscriber => {
+  getDiagMessages(): Observable<string[]>{
+    return new Observable<string[]>(subscriber => {
       this.db.database.ref("/diagnostica").on("child_added", child =>{
-        subscriber.next(child.val());
+        subscriber.next([child.key,child.val()]);
       });
     });
   }
 
+  deleteKey(key){
+    this.db.database.ref("/diagnostica/" + key).remove().then(() => {
+      console.log("Database key:" + key + " removal complete.");
+    }).catch(reason => {
+      console.log("Database removal FAILED reason: " + reason);
+    });
+  }
 
 }
--- src/app/diagnostica/diagnostica.component.ts
+++ src/app/diagnostica/diagnostica.component.ts
@@ -11,6 +11,7 @@ import {DiagnosticaService} from "../diagnostica.service";
 })
 export class DiagnosticaComponent implements OnInit {
   messagesList: Array<string> = ['root'];
+  messageMap = new Map();//Map del tipo <Messaggio>:<Key>
   constructor(public auth: AngularFireAuth, private _diagService: DiagnosticaService, private ngZone: NgZone) {
     this.checkIfUserIsLogged();
   }
@@ -23,16 +24,24 @@ export class DiagnosticaComponent implements OnInit {
     this.messagesList.pop();
     this._diagService.getDiagMessages().subscribe(value => {
       this.ngZone.run(() => {
-        this.messagesList.push(value);
+        this.messagesList.push(value[1]);
       })
+      this.messageMap.set(value[1],value[0]); //L'array conserva l'associazione key-value del db per la cancellazione
       console.log("added "+value);
     });
   }
 
   deleteMessages(object){
-
+    let index: number;
     for(let i of object){
-      console.log(i._text.nativeElement.innerText);
+      //console.log(i._text.nativeElement.innerText);
+      //console.log(this.messageMap.get(i._text.nativeElement.innerText));
+      this._diagService.deleteKey(this.messageMap.get(i._text.nativeElement.innerText));
+      //console.log("Eliminazione: " + this.messageMap.delete(i._text.nativeElement.innerText));
+      index = this.messagesList.indexOf(i._text.nativeElement.innerText);
+      this.ngZone.run(() => {
+        this.messagesList.splice(index,1);
+      });
     }
   }
 
