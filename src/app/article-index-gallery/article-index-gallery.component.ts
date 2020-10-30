b0ff219ea4d28d70c6de86c9c9db901a51901b7a
xerna01 <57638418+xerna01@users.noreply.github.com>
2020-10-30 18:23:15 +0100

    Fixati bug del merge

--- src/app/article-index-gallery/article-index-gallery.component.ts
+++ src/app/article-index-gallery/article-index-gallery.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import * as jQuery from 'jquery';
 import {MatDialog} from '@angular/material/dialog';
+import {MatDialogComponent} from '../mat-dialog/mat-dialog.component';
 
 
 
@@ -11,25 +12,22 @@ import {MatDialog} from '@angular/material/dialog';
   styleUrls: ['./article-index-gallery.component.css']
 })
 
-//Classe principale che contiene tutte le componenti della pagina
+// Classe principale che contiene tutte le componenti della pagina
 
-export class ArticleIndexGalleryComponent implements OnInit {
+export class ArticleIndexGalleryComponent implements OnInit, AfterViewInit {
 
   constructor(private matDialog: MatDialog) {}
 
   ngOnInit(): void {
   }
   ngAfterViewInit(){
-    /*alert("Attenzione! Non è stata ancora implementata una funzionalità di resizing automatico." +
-      " Per ottenere una visualizzazione ottimale della schermata, modificare la grandezza degli elementi con " +
-      "la combinazione di tasti Ctrl + & Ctrl - ");*/
-      if(localStorage.getItem("mostraResize") == "true")
+      if (localStorage.getItem('mostraResize') === 'true')
       {
         const choice = this.showChoiceDialog('La seguente applicazione è ottimizzata per schermi di 32\'\'.',
-        ["Se necessario, effettuare un resize (zoom out) tramite browser della pagina mostrata."]);
+        ['Se necessario, effettuare un resize (zoom out) tramite browser della pagina mostrata.']);
         choice.afterClosed().subscribe(result => {
         if (result === 1){
-         localStorage.setItem("mostraResize","false");
+         localStorage.setItem('mostraResize', 'false');
         }
       });
       }
